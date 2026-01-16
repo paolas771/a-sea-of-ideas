2020dc2c71e3b4e43db3367013ec4d5a4ef74469
Paola Silva <paolas771@gmail.com>
2026-01-16 13:36:44 -0600

    add more images to blog and remove quotes from blockquotes

--- src/app/Components/BlogImage.js
+++ src/app/Components/BlogImage.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
 
-export default function BlogImage({img, alt}){
+export default function BlogImage({img, alt, width, height}){
     // console.log(img)
     return (
         <Image 
             src={img}
-            width={200}
-            height={105}
+            width={width}
+            height={height}
             alt={alt}
+            className="content_image"
         />
     )
 }
