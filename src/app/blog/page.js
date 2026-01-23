f2e8d5e96d77da0f2d8d6e4ef2013d23d36eea71
Paola Silva <paolas771@gmail.com>
2026-01-22 18:44:49 -0600

    remove blog container component;

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -2,7 +2,6 @@ import Image from "next/image";
 import { promises as fs } from 'fs';
 // import data from '@/app/'
 import Link from 'next/link'
-import BlogContainer from '@/app/Components/BlogContainer'
 export default async function Home() {
     const file = await fs.readFile(process.cwd() + '/src/app/data.json');
     const data = JSON.parse(file);
@@ -18,7 +17,29 @@ export default async function Home() {
           <li>Coding</li>
         </ul>
       </section>
-      <BlogContainer data={data}/>
+      <section className="all_post">
+        {
+            data.map((blog, i) => {
+            return(
+                <div className="post" key={i}>
+                <Image 
+                    src={blog.image}
+                    width={200}
+                    height={75}
+                    alt={blog.alt}
+                    className="blog_image"
+                />
+                <Link href={`/blog/${blog.slug}`} className="blog_summary">
+                    <h5 className="tag">{blog.tag}</h5>
+                    <h2 className="blog_title">{blog.title}</h2>
+                    <p className="blog_description">{blog.description}</p>
+                    {/* <button>Read More</button> */}
+                </Link>
+                </div>
+            )
+            })
+        }
+        </section>
     </div>
 );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Welcome from '@/markdown/welcome.mdx'
 import Link from 'next/link'
-import BlogContainer from '@/app/Components/BlogContainer'
 import { promises as fs } from 'fs';
 
 export default async function Home() {
@@ -14,7 +13,29 @@ export default async function Home() {
         </article>
         <div>
           <h2 className="latest_blog">Latest Blogs</h2>
-          <BlogContainer data={data}/>
+          <section className="all_post">
+                      {
+                          data.map((blog, i) => {
+                          return(
+                              <div className="post" key={i}>
+                              <Image 
+                                  src={blog.image}
+                                  width={200}
+                                  height={75}
+                                  alt={blog.alt}
+                                  className="blog_image"
+                              />
+                              <Link href={`/blog/${blog.slug}`} className="blog_summary">
+                                  <h5 className="tag">{blog.tag}</h5>
+                                  <h2 className="blog_title">{blog.title}</h2>
+                                  <p className="blog_description">{blog.description}</p>
+                                  {/* <button>Read More</button> */}
+                              </Link>
+                              </div>
+                          )
+                          })
+                      }
+                  </section>
           <Link href="/blog" className="view_all">View All Blogs</Link>
         </div>
     </div>
